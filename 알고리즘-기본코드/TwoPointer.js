6ad61f56cd28a9e9861a3f7aa412d2807553113f
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-24 16:50:30 +0900

    투포인터 알고리즘 기 코드 - js

---
+++ 알고리즘-기본코드/TwoPointer.js
@@ -0,0 +1,20 @@
+function solution(n, arr) {
+  let answer = 0;
+  let sum = 0;
+  let end = 0;
+
+  for (let start = 0; start < arr.length; start += 1) {
+    while (sum < n && end < arr.length) {
+      sum += arr[end];
+      end += 1;
+    }
+    if (sum === n) {
+      answer += 1;
+    }
+    sum -= arr[start];
+  }
+  return answer;
+}
+
+const arr = [1, 2, 3, 2, 5];
+console.log(solution(5, arr));
