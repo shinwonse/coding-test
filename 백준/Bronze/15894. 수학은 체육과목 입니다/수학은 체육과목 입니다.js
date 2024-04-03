82aa887834be14cc2b9641b68048d26b169ee828
Wonse Shin <62709718+shinwonse@users.noreply.github.com>
2024-04-03 22:07:10 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 132 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.js
@@ -0,0 +1,10 @@
+const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs").readFileSync(path).toString().trim().split("\n");
+
+const solution = (input) => {
+  const n = Number(input[0]);
+  return n * 4;
+}
+
+const answer = solution(input);
+console.log(answer);
