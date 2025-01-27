5ad8c0664f52fd1be2c9e821590e4eae66e205cc
Wonse Shin <62709718+shinwonse@users.noreply.github.com>
2025-01-27 16:41:10 +0900

    [Silver IV] Title: ATM, Time: 100 ms, Memory: 9640 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.js
+++ 백준/Silver/11399. ATM/ATM.js
@@ -1,16 +1,22 @@
 const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(path).toString().trim().split("\n");
 
-const solution = (input) => {
-  let answer =0 ;
-  const N = input[0];
-  const arr = input[1].split(" ").map((n) => Number(n));
-  arr.sort((a, b) => a - b);
-  for (let i = 0; i < N ; i+= 1) {
-    answer += arr.slice(0, i + 1).reduce((acc, cur) => acc + cur, 0);
-  }
-  return answer;
-};
+function solution(input) {
+  const N = Number(input[0]);
+  const times = input[1]
+    .split(" ")
+    .map(Number)
+    .sort((a, b) => a - b);
 
-const answer = solution(input);
-console.log(answer);
+  let totalTime = 0;
+  let accumulatedTime = 0;
+
+  for (let i = 0; i < N; i++) {
+    accumulatedTime += times[i];
+    totalTime += accumulatedTime;
+  }
+
+  console.log(totalTime);
+}
+
+solution(input);
