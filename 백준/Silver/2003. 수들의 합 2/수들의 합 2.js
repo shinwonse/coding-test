bf0b497e5445bf060dd0b97840276e4c6680706e
Wonse Shin <62709718+shinwonse@users.noreply.github.com>
2025-02-14 22:36:18 +0900

    [Silver IV] Title: 수들의 합 2, Time: 156 ms, Memory: 9884 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.js
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.js
@@ -1,25 +1,31 @@
 const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(path).toString().trim().split("\n");
 
-const solution = (input) => {
-  const [n, m] = input[0].split(' ').map(Number);
-  const arr = input[1].split(' ').map(Number);
+function solution(input) {
+  const [N, M] = input[0].split(' ').map(Number);
+  const array = input[1].split(' ').map(Number);
 
-  let count = 0;
-  let intervalSum = 0;
+  let answer = 0;
+  
+  let start = 0;
   let end = 0;
+  let sum = 0;
 
-  for (let start = 0; start < n; start++) {
-    while (intervalSum < m && end < n) {
-      intervalSum += arr[end];
-      end += 1;
+  while (end <= N) {
+    if (sum < M) {
+      sum += array[end];
+      end++;
+    } else if (sum > M) {
+      sum -= array[start];
+      start++;
+    } else {
+      answer++;
+      sum -= array[start];
+      start++;
     }
-    if (intervalSum === m) {
-      count += 1;
-    }
-    intervalSum -= arr[start];
   }
-  console.log(count);
+
+  console.log(answer);
 }
 
 solution(input);
