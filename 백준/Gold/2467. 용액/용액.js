6b6311fbc3ff7b816751520d7c68b2dfdb3a5a71
Wonse Shin <62709718+shinwonse@users.noreply.github.com>
2025-02-16 17:48:12 +0900

    [Gold V] Title: 용액, Time: 196 ms, Memory: 26852 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.js
+++ 백준/Gold/2467. 용액/용액.js
@@ -1,26 +1,35 @@
 const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(path).toString().trim().split("\n");
 
-// 같은 양의 두 용액을 혼합한 용액의 특성값은 혼합에 사용된 각 용액의 특성값의 합으로 정의
-// 같은 양의 두 용액을 혼합하여 특성값이 0에 가장 가까운 용액을 만들려고 함
-// N개의 용액이 주어졌을 때, 특성값이 0에 가장 가까운 용액을 만드는 두 용액을 찾는 프로그램 작성
-const solution = (input) => {
-  const n = Number(input[0]);
-  const arr = input[1].split(' ').map(Number);
+function solution(input) {
+  const N = Number(input[0]);
+  const liquids = input[1].split(" ").map(Number);
 
-  let [left, right, min, output] = [0, n - 1, Infinity, ''];
+  let left = 0;
+  let right = N - 1;
+  let minSum = Infinity;
+  let answer = [];
 
   while (left < right) {
-    const sum = arr[left] + arr[right];
-    if (Math.abs(sum) < min) {
-      min = Math.abs(sum);
-      output = `${arr[left]} ${arr[right]}`;
+    const sum = liquids[left] + liquids[right];
+
+    // 0에 더 가까운 값이면 갱신
+    if (Math.abs(sum) < minSum) {
+      minSum = Math.abs(sum);
+      answer = [liquids[left], liquids[right]];
     }
-    sum < 0 ? left++ : right--;
+
+    // 합이 0보다 크면 sum을 줄이기 위해 right를 줄임
+    if (sum > 0) {
+      right--;
+    }
+    // 합이 0보다 작으면 sum을 키우기 위해 left를 증가
+    else {
+      left++;
+    }
   }
 
-  return output;
+  console.log(answer.join(" "));
 }
 
-const answer = solution(input);
-console.log(answer);
+solution(input);
