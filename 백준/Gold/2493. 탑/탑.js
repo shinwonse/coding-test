8022dd8dd2588d4821dcde3bc1350aa34abe0238
Wonse Shin <62709718+shinwonse@users.noreply.github.com>
2025-02-20 21:48:55 +0900

    [Gold V] Title: 탑, Time: 420 ms, Memory: 101324 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.js
+++ 백준/Gold/2493. 탑/탑.js
@@ -1,27 +1,23 @@
 const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(path).toString().trim().split("\n");
 
-const solution = (input) => {
-  const n = Number(input[0]);
+/**
+ * 각각의 탑에서 발사한 레이저 신호를 어느 탑에서 수신하는지
+ */
+function solution(input) {
+  const N = Number(input[0]);
   const towers = input[1].split(' ').map(Number);
-
-  const answer = [];
+  const answer = new Array(N).fill(0);
   const stack = [];
 
-  for (let i = 0; i < n; i++) {
-    const now = towers[i];
-
-    while (stack.length && towers[stack[stack.length - 1]] < now) {
-      stack.pop();
-    }
-
-    if (stack.length) {
-      answer.push(stack[stack.length - 1] + 1);
-    } else {
-      answer.push(0);
+  for (let i = N - 1; i >= 0; i--) {
+    while (stack.length && towers[stack[stack.length - 1]] <= towers[i]) {
+      const idx = stack.pop();
+      answer[idx] = i + 1;
     }
     stack.push(i);
   }
+
   console.log(answer.join(' '));
 }
 
