415afcb1e634263d3583ea2d763eba65ead632b8
Wonse Shin <62709718+shinwonse@users.noreply.github.com>
2025-02-24 23:14:54 +0900

    [Silver I] Title: 숨바꼭질, Time: 3632 ms, Memory: 20608 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,51 +1,38 @@
-const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = require("fs").readFileSync(path).toString().trim().split("\n");
+const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(path).toString().trim().split('\n');
 
-class Queue {
-  constructor() {
-    this.items = {};
-    this.headIndex = 0;
-    this.tailIndex = 0;
-  }
-  enqueue(item) {
-    this.items[this.tailIndex] = item;
-    this.tailIndex++;
-  }
-  dequeue() {
-    const item = this.items[this.headIndex];
-    delete this.items[this.headIndex];
-    this.headIndex++;
-    return item;
-  }
-  peek() {
-    return this.items[this.headIndex];
-  }
-  getLength() {
-    return this.tailIndex - this.headIndex;
-  }
+/**
+ * 최소 이동 횟수를 구하는 문제이므로 BFS
+ * BFS는 최단 경로를 보장
+ */
+function solution(input) {
+  const [N, K] = input[0].split(' ').map(Number);
+
+  const MAX = 100000;
+  const visited = new Array(MAX + 1).fill(0);
+
+  const answer = bfs(N, K, visited, MAX);
+  console.log(answer);
 }
 
-const solution = (input) => {
-  const [n, k] = input[0].split(' ').map(Number);
-  const visited = new Array(100001).fill(0);
+function bfs(cur, destination, visited, MAX) {
+  const queue = [[cur, 0]]; // (현재 위치, 이동 시간)
 
-  const bfs = () => {
-    const queue = new Queue();
-    queue.enqueue(n);
-    while (queue.getLength() !== 0) {
-      const cur = queue.dequeue();
-      if (cur === k) return visited[cur];
-      for (const next of [cur - 1, cur + 1, cur * 2]) {
-        if (next < 0 || next > 100000) continue;
-        if (visited[next] !== 0) continue;
-        visited[next] = visited[cur] + 1;
-        queue.enqueue(next);
+  while (queue.length) {
+    const [currentPosition, currentTime] = queue.shift();
+
+    // 목표 위치에 도달하면 즉시 반환
+    if (currentPosition === destination) {
+      return currentTime;
+    }
+
+    for (const next of [currentPosition - 1, currentPosition + 1, currentPosition * 2]) {
+      if (next >= 0 && next <= MAX && visited[next] === 0) {
+        visited[next] = 1; // 방문 체크
+        queue.push([next, currentTime + 1]);
       }
     }
   }
+}
 
-  return bfs();
-};
-
-const answer = solution(input);
-console.log(answer);
+solution(input);
