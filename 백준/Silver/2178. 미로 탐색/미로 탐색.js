bfa2ac8f5e85fbdd92754b314c538e32a963a419
Wonse Shin <62709718+shinwonse@users.noreply.github.com>
2025-02-24 22:40:43 +0900

    [Silver I] Title: 미로 탐색, Time: 168 ms, Memory: 11256 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -1,55 +1,35 @@
-const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = require("fs").readFileSync(path).toString().trim().split("\n");
+const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(path).toString().trim().split('\n');
 
-class Queue {
-  items = [];
-  front = 0;
-  rear = 0;
+const dx = [-1, 0, 1, 0];
+const dy = [0, -1, 0, 1];
 
-  push(item) {
-    this.items.push(item);
-    this.rear++;
-  }
+function solution(input) {
+  const [N, M] = input[0].split(' ').map(Number);
+  const graph = input.slice(1).map((row) => row.split('').map(Number));
 
-  pop() {
-    return this.items[this.front++];
-  }
-
-  isEmpty() {
-    return this.front === this.rear;
-  }
+  const answer = bfs(0, 0, graph, N, M);
+  console.log(answer);
 }
 
-const solution = (input) => {
-  const [n, m] = input[0].split(' ').map(Number);
-  const inputArr = input.slice(1,);
-  const graph = inputArr.map(v => v.split('').map(Number));
-  const visited = Array.from({ length: n }, () => Array(m).fill(0));
+function bfs(x, y, graph, n, m) {
+  const queue = [[x, y]];
 
-  const bfs = (x, y) => {
-    const dx = [0, 0, 1, -1];
-    const dy = [1, -1, 0, 0];
-    const queue = new Queue();
-    queue.push([x, y]);
-    visited[x][y] = 1;
+  while (queue.length) {
+    const [cx, cy] = queue.shift();
 
-    while (!queue.isEmpty()) {
-      const [x, y] = queue.pop();
-      for (let i = 0; i < 4; i += 1) {
-        const [nx, ny] = [x + dx[i], y + dy[i]];
-        if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-        if (graph[nx][ny] && !visited[nx][ny]) {
-          visited[nx][ny] = visited[x][y] + 1;
-          queue.push([nx, ny]);
+    for (let i = 0; i < 4; i++) {
+        const nx = cx + dx[i];
+        const ny = cy + dy[i];
+
+        if (nx >= 0 && nx < n && ny >= 0 && ny < m && graph[nx][ny] === 1) {
+            graph[nx][ny] = graph[cx][cy] + 1;
+            queue.push([nx, ny]);
         }
-      }
     }
   }
 
-  bfs(0, 0);
-  return visited[n - 1][m - 1];
+  return graph[n - 1][m - 1];
 }
 
-
-const answer = solution(input);
-console.log(answer);
+solution(input);
