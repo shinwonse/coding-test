49d83ed1ba6d8ef194a5ef4c60f8ad2cce8b8106
Wonse Shin <62709718+shinwonse@users.noreply.github.com>
2025-03-04 23:29:29 +0900

    [Silver III] Title: 주유소, Time: 192 ms, Memory: 19704 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,24 +1,20 @@
-const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = require("fs").readFileSync(path).toString().trim().split("\n");
+const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(path).toString().trim().split('\n');
 
-const solution = (input) => {
+function solution(input) {
   const N = Number(input[0]);
-  const distances = input[1].split(" ").map((v) => Number(v));
-  let costs = input[2].split(" ").map((v) => Number(v));
+  const roads = input[1].split(' ').map(Number);
+  const oils = input[2].split(' ').map(Number);
 
-  let minCost = costs[0];
-  for (let i = 0; i < N; i += 1) {
-    minCost = Math.min(minCost, costs[i]);
-    costs[i] = minCost;
+  let minPrice = oils[0];
+  let totalCost = 0;
+
+  for (let i = 0; i < N - 1; i++) {
+    minPrice = Math.min(minPrice, oils[i]);
+    totalCost += minPrice * roads[i];
   }
 
-  let answer = BigInt(0);
-  for (let i = 0; i < N - 1; i += 1) {
-    answer += BigInt(costs[i]) * BigInt(distances[i]);
-  }
+  console.log(totalCost);
+}
 
-  return String(answer);
-};
-
-const answer = solution(input);
-console.log(answer);
+solution(input);
