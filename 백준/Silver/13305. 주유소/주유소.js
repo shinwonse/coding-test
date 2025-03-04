091bfc22dcb1d577599004b9adad83bdc94b0419
Wonse Shin <62709718+shinwonse@users.noreply.github.com>
2025-03-04 23:36:13 +0900

    [Silver III] Title: 주유소, Time: 272 ms, Memory: 44464 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -3,18 +3,18 @@ const input = require('fs').readFileSync(path).toString().trim().split('\n');
 
 function solution(input) {
   const N = Number(input[0]);
-  const roads = input[1].split(' ').map(Number);
-  const oils = input[2].split(' ').map(Number);
+  const distances = input[1].split(' ').map(BigInt);
+  const prices = input[2].split(' ').map(BigInt);
 
-  let minPrice = oils[0];
-  let totalCost = 0;
+  let minPrice = prices[0]; // 첫 번째 주유소의 가격
+  let totalCost = BigInt(0); // BigInt로 선언
 
   for (let i = 0; i < N - 1; i++) {
-    minPrice = Math.min(minPrice, oils[i]);
-    totalCost += minPrice * roads[i];
+    minPrice = minPrice < prices[i] ? minPrice : prices[i]; // 최소값 갱신
+    totalCost += minPrice * distances[i]; // 최소 가격으로 거리만큼 주유
   }
 
-  console.log(totalCost);
+  console.log(totalCost.toString()); // BigInt는 문자열로 출력해야 함
 }
 
 solution(input);
