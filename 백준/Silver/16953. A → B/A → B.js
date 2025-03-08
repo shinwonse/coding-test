c1f8a71caa2dca33db2ad9fa583f1072877b784a
Wonse Shin <62709718+shinwonse@users.noreply.github.com>
2025-03-08 13:41:38 +0900

    [Silver II] Title: A → B, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.js
+++ 백준/Silver/16953. A → B/A → B.js
@@ -1,52 +1,24 @@
-const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = require("fs").readFileSync(path).toString().trim().split("\n");
+const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(path).toString().trim().split('\n');
 
-class Queue {
-  constructor() {
-    this.items = {};
-    this.headIndex = 0;
-    this.tailIndex = 0;
-  }
-  enqueue(item) {
-    this.items[this.tailIndex] = item;
-    this.tailIndex++;
-  }
-  dequeue() {
-    const item = this.items[this.headIndex];
-    delete this.items[this.headIndex];
-    this.headIndex++;
-    return item;
-  }
-  peek() {
-    return this.items[this.headIndex];
-  }
-  getLength() {
-    return this.tailIndex - this.headIndex;
-  }
-}
+function solution(input) {
+  let [A, B] = input[0].split(' ').map(Number);
 
-const solution = (input) => {
-  const [a, b] = input[0].split(' ').map(Number);
-  const queue = new Queue();
-  queue.enqueue([a, 0]);
-  const visited = new Set();
-  let found = false;
+  let count = 1;
 
-  while (queue.getLength() !== 0) {
-    const [n, count] = queue.dequeue();
-    if (n === b) {
-      found = true;
-      return count + 1;
+  while (B > A) {
+    if (B % 2 === 0) {
+      B = B / 2;
+    } else if (B % 10 === 1) {
+      B = Math.floor(B / 10);
+    } else {
+      console.log(-1);
+      return;
     }
-    if (visited.has(n)) continue;
-    visited.add(n);
-    if (n * 2 <= b) queue.enqueue([n * 2, count + 1]);
-    if (n * 10 + 1 <= b) queue.enqueue([n * 10 + 1, count + 1]);
+    count++;
   }
-  if (!found) {
-    return -1;
-  }
+
+  console.log(B === A ? count: -1);
 }
 
-const answer = solution(input);
-console.log(answer);
+solution(input);
