ea547321d842a8b2173990bba080c2b382a60f95
Wonse Shin <62709718+shinwonse@users.noreply.github.com>
2025-03-21 22:27:06 +0900

    [Silver I] Title: 숨바꼭질, Time: 3572 ms, Memory: 19828 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,35 +1,27 @@
 const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = require('fs').readFileSync(path).toString().trim().split('\n');
 
-/**
- * 최소 이동 횟수를 구하는 문제이므로 BFS
- * BFS는 최단 경로를 보장
- */
 function solution(input) {
   const [N, K] = input[0].split(' ').map(Number);
-
   const MAX = 100000;
-  const visited = new Array(MAX + 1).fill(0);
 
-  const answer = bfs(N, K, visited, MAX);
-  console.log(answer);
-}
-
-function bfs(cur, destination, visited, MAX) {
-  const queue = [[cur, 0]]; // (현재 위치, 이동 시간)
+  const visited = Array(MAX + 1).fill(false);
+  const queue = [];
+  queue.push([N, 0]);
+  visited[N] = true;
 
   while (queue.length) {
-    const [currentPosition, currentTime] = queue.shift();
+    const [pos, time] = queue.shift();
 
-    // 목표 위치에 도달하면 즉시 반환
-    if (currentPosition === destination) {
-      return currentTime;
+    if (pos === K) {
+      console.log(time);
+      return;
     }
 
-    for (const next of [currentPosition - 1, currentPosition + 1, currentPosition * 2]) {
-      if (next >= 0 && next <= MAX && visited[next] === 0) {
-        visited[next] = 1; // 방문 체크
-        queue.push([next, currentTime + 1]);
+    for (const next of [pos - 1, pos + 1, pos * 2]) {
+      if (next >= 0 && next <= MAX && !visited[next]) {
+        visited[next] = true;
+        queue.push([next, time + 1]);
       }
     }
   }
