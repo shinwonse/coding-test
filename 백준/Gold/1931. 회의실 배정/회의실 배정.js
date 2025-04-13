9bbd3c64253006955523281d32407529a0858427
Wonse Shin <62709718+shinwonse@users.noreply.github.com>
2025-04-13 19:53:16 +0900

    [Gold V] Title: 회의실 배정, Time: 344 ms, Memory: 41888 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.js
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.js
@@ -1,32 +1,29 @@
-const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = require("fs").readFileSync(path).toString().trim().split("\n");
+const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(path).toString().trim().split('\n');
 
 function solution(input) {
   const N = Number(input[0]);
-  const arr = [];
+  const meets = input
+    .slice(1)
+    .map((meet) => meet.split(' ').map(Number))
+    .sort((a, b) => {
+      if (a[1] === b[1]) {
+        return a[0] - b[0];
+      } else {
+        return a[1] - b[1];
+      }
+    });
 
-  for (let i = 1; i <= N; i++) {
-    const [start, end] = input[i].split(" ").map(Number);
-    arr.push([start, end]);
-  }
+    let answer = 0;
+    let current = 0;
+    meets.forEach((meet) => {
+      if (meet[0] >= current) {
+        answer++;
+        current = meet[1];
+      }
+    });
 
-  arr.sort((a, b) => {
-    if (a[1] === b[1]) return a[0] - b[0];
-    return a[1] - b[1];
-  });
-
-  let answer = 0;
-  let currentTime = 0;
-
-  for (let i = 0; i < N; i++) {
-    const [start, end] = arr[i];
-    if (currentTime <= start) {
-      answer++;
-      currentTime = end;
-    }
-  }
-
-  console.log(answer);
+    console.log(answer);
 }
 
 solution(input);
