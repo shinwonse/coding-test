928dd51f34258dd404e5543cd5cbdd9b20009e2a
Wonse Shin <62709718+shinwonse@users.noreply.github.com>
2025-05-03 20:45:48 +0900

    [Silver II] Title: 연속합, Time: 168 ms, Memory: 15284 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.js
+++ 백준/Silver/1912. 연속합/연속합.js
@@ -1,19 +1,20 @@
-const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = require("fs").readFileSync(path).toString().trim().split("\n");
+const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(path).toString().trim().split('\n');
 
 function solution(input) {
   const n = Number(input[0]);
-  const nums = input[1].split(" ").map(Number);
+  const array = input[1].split(' ').map(Number);
 
-  let currentMax = nums[0];
-  let globalMax = nums[0];
+  const dp = Array(n).fill(0);
+  dp[0] = array[0];
+  let maxSum = dp[0];
 
   for (let i = 1; i < n; i++) {
-    currentMax = Math.max(currentMax + nums[i], nums[i]);
-    globalMax = Math.max(globalMax, currentMax);
+    dp[i] = Math.max(dp[i - 1] + array[i], array[i]);
+    maxSum = Math.max(maxSum, dp[i]);
   }
 
-  console.log(globalMax);
+  console.log(maxSum);
 }
 
 solution(input);
